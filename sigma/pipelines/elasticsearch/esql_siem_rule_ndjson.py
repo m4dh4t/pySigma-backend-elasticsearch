6012efb00ef2348657f9c565834d46c7a94e74a1
Yann Pellegrin <yann.pellegrin@hacknowledge.ch>
2024-09-10 10:41:20 +0200

    wip: reformat

--- sigma/pipelines/elasticsearch/esql_siem_rule_ndjson.py
+++ sigma/pipelines/elasticsearch/esql_siem_rule_ndjson.py
@@ -54,50 +54,52 @@ from sigma.processing.postprocessing import QueryTemplateTransformation
 """
 
 
-
 def esql_siem_rule_ndjson() -> ProcessingPipeline:
     return ProcessingPipeline(
         postprocessing_items=[
             QueryPostprocessingItem(
                 transformation=QueryTemplateTransformation(
-                    dedent("""
-                    {
-                        "id": "{{ rule.id }}",
-                        "name": "SIGMA - {{ rule.title }}",
-                        "tags":  {% if rule.tags is not none %}[{% for tag in rule.tags %}"{{ tag.namespace }}-{{ tag.name }}"{% if not loop.last %},{% endif %}{% endfor %}]{% endif %},
-                        "interval": "FIXME",
-                        "enabled": true,
-                        "description": {{ rule.description if rule.description is not none else "No description" }},
-                        "risk_score": {{ FIXME if rule.level is not none else 21 }},
-                        "severity": "{{ rule.level.name.lower() if rule.level is not none else "low" }}",
-                        "note": "",
-                        "license": "DRL",
-                        "output_index": "",
-                        "meta": {
-                            "from": "1m",
-                        },
-                        "investigation_fields": {},
-                        "author": {{ rule.author if rule.author is not none else [] }},
-                        "false_positives": {{ rule.falsepositives }},
-                        "from": "FIXME",
-                        "rule_id": "{{ rule.id }}",
-                        "max_signals": 100,
-                        "risk_score_mapping": [],
-                        "severity_mapping": [],
-                        "threat": "FIXME",
-                        "to": "now",
-                        "references": {{ rule.references if rule.references is not none else [] }},
-                        "version": 1,
-                        "exceptions_list": [],
-                        "immutable": false,
-                        "related_integrations": [],
-                        "required_fields": [],
-                        "setup": "",
-                        "type": "esql",
-                        "language": "esql",
-                        "query": "{{ query }}",
-                        "actions": [],
-                    }""")
+                    dedent(
+                        """
+                        {
+                            "id": "{{ rule.id }}",
+                            "name": "SIGMA - {{ rule.title }}",
+                            "tags":  {% if rule.tags is not none %}[{% for tag in rule.tags %}"{{ tag.namespace }}-{{ tag.name }}"{% if not loop.last %},{% endif %}{% endfor %}]{% endif %},
+                            "interval": "FIXME",
+                            "enabled": true,
+                            "description": {{ rule.description if rule.description is not none else "No description" }},
+                            "risk_score": {{ FIXME if rule.level is not none else 21 }},
+                            "severity": "{{ rule.level.name.lower() if rule.level is not none else "low" }}",
+                            "note": "",
+                            "license": "DRL",
+                            "output_index": "",
+                            "meta": {
+                                "from": "1m",
+                            },
+                            "investigation_fields": {},
+                            "author": {{ rule.author if rule.author is not none else [] }},
+                            "false_positives": {{ rule.falsepositives }},
+                            "from": "FIXME",
+                            "rule_id": "{{ rule.id }}",
+                            "max_signals": 100,
+                            "risk_score_mapping": [],
+                            "severity_mapping": [],
+                            "threat": "FIXME",
+                            "to": "now",
+                            "references": {{ rule.references if rule.references is not none else [] }},
+                            "version": 1,
+                            "exceptions_list": [],
+                            "immutable": false,
+                            "related_integrations": [],
+                            "required_fields": [],
+                            "setup": "",
+                            "type": "esql",
+                            "language": "esql",
+                            "query": "{{ query }}",
+                            "actions": [],
+                        }
+                        """
+                    )
                 )
             ),
         ]
